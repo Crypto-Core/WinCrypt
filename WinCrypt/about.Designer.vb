9357cdea21b65e75020aca451dc35b0762f8088d
WinCrypt <support@wincrypt.org>
2015-07-21 18:11:59 +0200

    Lizenz in GPLv3 geändert

--- WinCrypt/about.Designer.vb
+++ WinCrypt/about.Designer.vb
@@ -24,11 +24,12 @@ Partial Class about
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(about))
         Me.product_lb = New System.Windows.Forms.Label()
-        Me.info_txt = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.version_lb = New System.Windows.Forms.Label()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.copyright_lb = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -41,17 +42,6 @@ Partial Class about
         Me.product_lb.Size = New System.Drawing.Size(166, 23)
         Me.product_lb.TabIndex = 1
         Me.product_lb.Text = "WinCrypt 1.2"
-        '
-        'info_txt
-        '
-        Me.info_txt.Location = New System.Drawing.Point(180, 12)
-        Me.info_txt.Multiline = True
-        Me.info_txt.Name = "info_txt"
-        Me.info_txt.ReadOnly = True
-        Me.info_txt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.info_txt.Size = New System.Drawing.Size(496, 188)
-        Me.info_txt.TabIndex = 3
-        Me.info_txt.Text = resources.GetString("info_txt.Text")
         '
         'PictureBox1
         '
@@ -69,7 +59,7 @@ Partial Class about
         Me.Button1.BackColor = System.Drawing.Color.Gainsboro
         Me.Button1.FlatAppearance.BorderSize = 0
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button1.Location = New System.Drawing.Point(320, 206)
+        Me.Button1.Location = New System.Drawing.Point(318, 244)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(95, 23)
         Me.Button1.TabIndex = 4
@@ -88,22 +78,40 @@ Partial Class about
         'LinkLabel1
         '
         Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.Location = New System.Drawing.Point(639, 203)
+        Me.LinkLabel1.Location = New System.Drawing.Point(639, 257)
         Me.LinkLabel1.Name = "LinkLabel1"
         Me.LinkLabel1.Size = New System.Drawing.Size(37, 13)
         Me.LinkLabel1.TabIndex = 6
         Me.LinkLabel1.TabStop = True
         Me.LinkLabel1.Text = "Lizenz"
         '
+        'copyright_lb
+        '
+        Me.copyright_lb.AutoSize = True
+        Me.copyright_lb.Location = New System.Drawing.Point(9, 257)
+        Me.copyright_lb.Name = "copyright_lb"
+        Me.copyright_lb.Size = New System.Drawing.Size(189, 13)
+        Me.copyright_lb.TabIndex = 7
+        Me.copyright_lb.Text = "Copyright (C) [Jahr]  [Name des Autors]"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(180, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(496, 226)
+        Me.RichTextBox1.TabIndex = 8
+        Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
+        '
         'about
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(688, 234)
+        Me.ClientSize = New System.Drawing.Size(688, 279)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.copyright_lb)
         Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.version_lb)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.info_txt)
         Me.Controls.Add(Me.product_lb)
         Me.Controls.Add(Me.PictureBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
@@ -117,8 +125,9 @@ Partial Class about
     End Sub
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents product_lb As System.Windows.Forms.Label
-    Friend WithEvents info_txt As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents version_lb As System.Windows.Forms.Label
     Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents copyright_lb As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
 End Class
--- WinCrypt/about.vb
+++ WinCrypt/about.vb
@@ -4,6 +4,7 @@
     End Sub
 
     Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        copyright_lb.Text = "Copyright (C) " & System.DateTime.Today.Year & " Harry Machura"
         product_lb.Text = "WinCrypt " & My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor
         version_lb.Text = "(Version " & My.Application.Info.Version.ToString & ")"
         PictureBox1.Select()
