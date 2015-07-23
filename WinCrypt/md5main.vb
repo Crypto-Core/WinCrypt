7efd5a22f0a46ef5482c5eb03b3f3e263a5d3ae4
WinCrypt <support@wincrypt.org>
2015-07-23 22:43:07 +0200

    Test

--- WinCrypt/md5main.vb
+++ WinCrypt/md5main.vb
@@ -4,6 +4,7 @@ Imports System.Text
 Public Class md5main
     Dim i As New MD5
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles convert_bt.Click
+        'Test
         md5_txt.Text = i.MD5StringHash(text_txt.Text)
     End Sub
 End Class
