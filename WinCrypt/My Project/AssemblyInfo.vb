50933e3cd286bce4ebb2e52792074b2067068665
WinCrypt <support@wincrypt.org>
2015-07-30 22:29:58 +0200

    change version

--- WinCrypt/My Project/AssemblyInfo.vb
+++ WinCrypt/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.5.5")>
-<Assembly: AssemblyFileVersion("1.4.5.5")>
+<Assembly: AssemblyVersion("1.4.5.6")> 
+<Assembly: AssemblyFileVersion("1.4.5.6")> 
 <Assembly: NeutralResourcesLanguage("en-US")>
